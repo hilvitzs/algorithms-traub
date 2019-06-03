7ee38747b1575bf0afdef7e1004e26cc07bf3de0
Spencer Hilvitz <spencer.hilvitz@wipro.com>
2019-06-03 13:55:10 -0600

    Add comments to trie

--- trieTree.js
+++ trieTree.js
@@ -56,7 +56,7 @@ Trie.prototype.remove = function(word = '', node = this) {
   while (true) {
     child && parent && delete parent.children[child.value];
 
-    if (Object.keys(parent.children).length ||!chain.length) {
+    if (Object.keys(parent.children).length || !chain.length) {
       node.isWord = false;
       return node;
     };
@@ -79,3 +79,49 @@ Trie.prototype.hasWord = function(word) {
   const node = this.find(word);
   return !!node && node.isWord;
 };
+
+let trie = new Trie();
+
+// trie.add('add');
+// trie.add('words');
+// trie.add('please');
+
+// console.log(trie.hasWord('add')); // true
+// console.log(trie.hasWord('words')); // true
+// console.log(trie.hasWord('please')); // true
+
+// trie.add('trie');
+// trie.add('javascript');
+// trie.add('recursion');
+
+// console.log(trie.findWords().sort()); // ['javascript', 'recursion', 'trie']
+
+// trie.add('wow');
+// trie.add('word');
+// trie.add('wait');
+
+// console.log(trie.findWords('wo').sort()); // ['word', 'wow']
+
+trie.add('one');
+trie.add('two');
+trie.add('three');
+trie.add('forty-two');
+
+console.log(trie.findWords().sort()); // ['forty-two', 'one', 'three', 'two']
+
+trie.remove('forty-two');
+
+console.log(trie.findWords().sort()); // ['one', 'three', 'two']
+
+// trie.add('one');
+// trie.add('once');
+// trie.add('only');
+
+// console.log(trie.findWords().sort()); // ['once', 'one', 'only']
+
+// trie.remove('onl');
+
+// console.log(trie.findWords().sort()); // ['once', 'one', 'only']
+
+
+
