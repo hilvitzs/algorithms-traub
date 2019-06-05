9232b07989c63d8f509931548f5cadc9d6f1e0dc
Spencer Hilvitz <spencer.hilvitz@wipro.com>
2019-06-05 14:53:00 -0600

    Change trie tree to different implementation

--- trieTree.js
+++ trieTree.js
@@ -1,127 +1,80 @@
-function Trie(letter = '') {
-  this.value = letter;
-  this.children = {};
-  this.isWord = false;
+class Trie {
+  constructor() {
+    this.root = new TrieNode(null);
+  }
+
+  insert(input) {
+    let node = this.root;
+
+    for (let i = 0; i < input.length; i++) {
+      if (!node.children[input[i]]) {
+        node.children[input[i]] = new TrieNode(input[i]);
+        node.children[input[i]].parent = node;
+      }
+
+      node = node.children[input[i]];
+
+      if (i === input.length - 1) {
+        node.end = true;
+      }
+    }
+  }
+
+  find(input) {
+    let node = this.root;
+    let output = [];
+
+    for (let i = 0; i < input.length; i++) {
+      if (!node.children[input[i]]) {
+        return output;
+      } else {
+        node = node.children[input[i]];
+      }
+    }
+
+    findAllWords(node, output);
+
+    return output;
+  }
 }
 
-Trie.prototype.add = function(word, node = this) {
-  for (const letter of word) {
-    if (node.children[letter]) {
-      node = node.children[letter];
-    } else {
-      const newNode = new Trie(letter);
-      node.children[letter] = newNode;
-      node = newNode;
-    };
-  };
+class TrieNode {
+  constructor(key) {
+    this.key = key;
+    this.parent = null;
+    this.children = {};
+    this.end = false;
+  }
 
-  node.isWord = true;
-};
+  getWord() {
+    let node = this;
+    let output = [];
 
-Trie.prototype.find = function(word, node = this) {
-  let value = '';
+    while (node !== null) {
+      output.unshift(node.key);
+      node = node.parent;
+    }
 
-  for (const letter of word) {
-    if (node.children[letter]) {
-      node = node.children[letter];
-      value += letter;
-    };
-  };
+    return output.join('');
+  }
+}
 
-  return value === word ? node : null;
-};
+const findAllWords = (node, arr) => {
+  if (node.end) {
+    return arr.unshift(node.getWord())
+  }
 
-Trie.prototype.remove = function(word = '', node = this) {
-  if (!word) return null;
-
-  const chain = [];
-
-  for (const letter of word) {
-    if (node.children[letter]) {
-      chain.push(node);
-      node = node.children[letter];
-    } else {
-      return null;
-    };
-  };
-
-  if (Object.keys(node.children).length) {
-    node.isWord = false;
-    return node;
-  };
-
-  let child = chain.length ? chain.pop() : null;
-  let parent = chain.length ? chain.pop() : null;
-
-  while (true) {
-    child && parent && delete parent.children[child.value];
-
-    if (Object.keys(parent.children).length || !chain.length) {
-      node.isWord = false;
-      return node;
-    };
-
-    child = parent;
-    parent = chain.pop();
-  };
-};
-
-Trie.prototype.findWords = function(value = '', node = this.find(value), words = []) {
-  Object.values(node.children).forEach(child => {
-    if (child.isWord) words.push(value + child.value);
-    child.findWords(value + child.value, child, words);
-  });
-
-  return words;
-};
-
-Trie.prototype.hasWord = function(word) {
-  const node = this.find(word);
-  return !!node && node.isWord;
-};
+  for (let child in node.children) {
+    findAllWords(node.children[child], arr);
+  }
+}
 
 let trie = new Trie();
 
-// trie.add('add');
-// trie.add('words');
-// trie.add('please');
+// insert few values
+trie.insert("hello");
+trie.insert("helium");
 
-// console.log(trie.hasWord('add')); // true
-// console.log(trie.hasWord('words')); // true
-// console.log(trie.hasWord('please')); // true
-
-// trie.add('trie');
-// trie.add('javascript');
-// trie.add('recursion');
-
-// console.log(trie.findWords().sort()); // ['javascript', 'recursion', 'trie']
-
-// trie.add('wow');
-// trie.add('word');
-// trie.add('wait');
-
-// console.log(trie.findWords('wo').sort()); // ['word', 'wow']
-
-trie.add('one');
-trie.add('two');
-trie.add('three');
-trie.add('forty-two');
-
-console.log(trie.findWords().sort()); // ['forty-two', 'one', 'three', 'two']
-
-trie.remove('forty-two');
-
-console.log(trie.findWords().sort()); // ['one', 'three', 'two']
-
-// trie.add('one');
-// trie.add('once');
-// trie.add('only');
-
-// console.log(trie.findWords().sort()); // ['once', 'one', 'only']
-
-// trie.remove('onl');
-
-// console.log(trie.findWords().sort()); // ['once', 'one', 'only']
-
-
-
+// check find method
+console.log('first', trie.find("hel"));  // [ 'helium', 'hello' ]
+console.log('second', trie.find("hell")); // [ 'hello' ]
